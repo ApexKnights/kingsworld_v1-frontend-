25e1dfadd62127f37290a0e94627273516ed69f3
Souparno <souparna2000shee@gmail.com>
2024-12-30 19:39:53 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ContextProvider } from './context/useContext.jsx'
 
 
@@ -12,9 +12,9 @@ export const server = "https://kingsworldv1-backend.onrender.com/api/v1"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContextProvider>
   </StrictMode>,
 )
